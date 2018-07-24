a24e4c8bde052eaf28c2ac8398784d3e45ab2842
Anna <anna.cantrell@crisp.agency>
2018-07-24 16:01:00 -0400

    new styles

--- header.php
+++ header.php
@@ -20,6 +20,45 @@
 </head>
 <body <?php body_class(); ?>>
 
-	<?php wp_nav_menu(
-		array( 'theme_location' => 'main-menu' )
-	); ?>
+	<section id="header">
+	<div class="wrapper">
+		<div class="container">
+
+			<div id="titleContainer" class="title-container">
+				<h1 id="name">Anna Cantrell</h1>
+				<p id="tagline">design + develop + experience</p>
+			</div>
+
+			<div id="hr" class="nav-hr">
+			</div>
+
+				<?php wp_nav_menu( array(
+					'theme_location' => 'main-menu',
+					'menu_class' => 'nav-bar'
+				 )
+				); ?>
+				
+		</div>
+	</div>
+</section>
+
+
+<section id="hero">
+	<div class=" wrapper">
+		<div class="container">
+
+			<div class="geometry-container">
+				<div id="rhom" class="rhom-container">
+					<div class="geo-rhom-outer"></div>
+					<div class="geo-rhom-inner"></div>
+				</div>
+
+					<div id="par" class="par-container">
+						<div class="geo-par-1"></div>
+						<div class="geo-par-2"></div>
+					</div>
+			</div>
+
+		</div>
+	</div>
+</section>
